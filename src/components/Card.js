fdb23e6df15023742f860ec2802fe2b9dd7b0705
Andrej Bozic <abozic389@gmail.com>
2022-04-28 15:02:59 +0200

    Card shuffling and score update logic.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import "../styles/Card.scss";
 
 function Card(props) {
-  const { title, img } = props;
+  const {
+    title, img, click, id,
+  } = props;
+
+  const clickHandler = (e) => {
+    const elementId = e.target.closest("div").getAttribute("id");
+    click(elementId);
+  };
+
   return (
-    <div className="card-container">
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div className="card-container" onClick={clickHandler} id={id}>
       <img src={img} alt="" className="card-img" />
       <h2>{title}</h2>
     </div>
--- src/components/CardBoard.js
+++ src/components/CardBoard.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import uniqid from "uniqid";
 import Card from "./Card";
 import {
@@ -16,68 +16,130 @@ import {
 } from "../img/index";
 import "../styles/CardBoard.scss";
 
-const cards = [
+const cardsTemplate = [
   {
     id: uniqid(),
+    clicked: false,
     title: "Jake Peralta",
     img: PeraltaImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Amy Santiago",
     img: SantiagoImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Rosa Diaz",
     img: DiazImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Raymond Holt",
     img: HoltImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Charles Boyle",
     img: BoyleImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Terry Jeffords",
     img: TerryImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Norm Scully",
     img: ScullyImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Michael Hitchcock",
     img: HitchcockImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Adrian Pimento",
     img: PimentoImg,
   },
   {
     id: uniqid(),
+    clicked: false,
     title: "Gina Linetti",
     img: GinaImg,
   },
 ];
+
+function shuffle(array) {
+  const output = [...array];
+  let m = array.length;
+  let t;
+  let i;
+
+  while (m) {
+    i = Math.floor(Math.random() * m);
+    m -= 1;
+    t = output[m];
+    output[m] = output[i];
+    output[i] = t;
+  }
+
+  return output;
+}
 
 function CardBoard(props) {
   const {
     setHighScore, setScore, highScore, score,
   } = props;
 
+  const [cards, setCards] = useState(cardsTemplate);
+
+  useEffect(() => {
+    setCards(shuffle([...cards]));
+  }, []);
+
+  const gameOver = () => {
+    if (score > highScore) setHighScore(score);
+    const newCards = cards.map((card) => {
+    // eslint-disable-next-line no-param-reassign
+      card.clicked = false;
+      return card;
+    });
+    setCards(shuffle(newCards));
+    setScore(0);
+  };
+
+  const clickHandler = (id) => {
+    let failed = false;
+    let newCards = [...cards];
+    newCards = newCards.map((card) => {
+      if (card.id === id) {
+        if (card.clicked) failed = true;
+        // eslint-disable-next-line no-param-reassign
+        else card.clicked = true;
+      }
+      return card;
+    });
+    if (failed) gameOver();
+    else {
+      setScore(score + 1);
+      setCards(shuffle(newCards));
+    }
+  };
+
   return (
     <div className="board-container">
       {cards.map((card) => (
-        <Card key={card.id} img={card.img} title={card.title} />
+        <Card key={card.id} img={card.img} title={card.title} click={clickHandler} id={card.id} />
       ))}
     </div>
   );
